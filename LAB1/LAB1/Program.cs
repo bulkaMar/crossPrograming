7df0ea023838cd446e9be615720b65ed7c40d0e5
Marina Bulakh <bulahmarina607@gmail.com>
2024-09-22 15:05:08 +0300

    fix file path

--- LAB1/LAB1/Program.cs
+++ LAB1/LAB1/Program.cs
@@ -3,13 +3,33 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
+/*
+    Задача полягає в тому, щоб знайти кількість різних перестановок (анаграм) даного слова з урахуванням повторень літер.
+
+    Алгоритм моїх дій:
+
+    1. Спочатку підраховую кількість кожної літери у слові. Якщо букви в слові повторюються, це зменшує кількість унікальних перестановок.
+    
+    2. Обчислюю факторіал від довжини слова. Тобто це факторіал від довжини слова n! дає загальну кількість перестановок, якби всі букви були б унікальними.
+
+    3. Далі ділю факторіал довжини слова на добуток факторіалів кількостей повторюваних літер. Тепер якщо літери повторюються, то кількість перестановок зменшується.
+    Ось така формула для кількості унікальних перестановок: P = n! / (k1! * k2! * ... * km!), n — це загальна кількість літер,
+    k1, k2, ..., km — це кількість кожної унікальної літери.
+
+    Для слова "solo" використовую формулу яку написала вище. 
+    Отже, довжина слова: n = 4.
+    Далі підраховую кількість кожної унікальної літери всі літери, окрім 'o' зустрічаються один раз.
+окрім 'o'
+    За формулою вище все пірахувала і вийшов такий результат: P = 4! / (1! * 2! * 1!) = 24 / 2 = 12.
+*/
+
 class Program
 {
     static void Main()
     {
-        string word = File.ReadAllText("INPUT.TXT").Trim();
-        Dictionary<char, int> letterCount = word.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        string word = File.ReadAllText("../../../../INPUT.TXT").Trim();
 
+        Dictionary<char, int> letterCount = word.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
         long Factorial(int n)
         {
             long result = 1;
@@ -24,6 +44,6 @@ class Program
         foreach (var count in letterCount.Values)
             totalPermutations /= Factorial(count);
 
-        File.WriteAllText("OUTPUT.TXT", totalPermutations.ToString());   
+        File.WriteAllText("../../../../OUTPUT.TXT", totalPermutations.ToString());
     }
 }
